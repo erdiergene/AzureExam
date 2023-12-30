41c4a08e7c9b533769a135ca1f9cc9976d359e82
erdi ergene <erdieergene@gmail.com>
2023-12-30 22:00:43 +0300

    Initial Commit

---
+++ AzureExam/AzureExamApp.swift
@@ -0,0 +1,17 @@
+//
+//  AzureExamApp.swift
+//  AzureExam
+//
+//  Created by Erdi Ergene on 30.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct AzureExamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
